4dde898436e2e189e125e1d2f55f1c42eb609255
sumela1988 <sumela.mixa@gmail.com>
2020-09-17 14:14:15 +0200

    3 files modified, 1 added

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule }    from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { FormsModule }   from '@angular/forms';
 
 // used to create fake backend
 import { fakeBackendProvider } from './_helpers';
@@ -13,12 +14,23 @@ import { JwtInterceptor, ErrorInterceptor } from './_helpers';
 import { HomeComponent } from './home';
 import { LoginComponent } from './login';
 
+import { AngularFireModule } from 'angularfire2';
+import { FirebaseConfig } from '../environments/firebase.config';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
 
 @NgModule({
     imports: [
         BrowserModule,
         ReactiveFormsModule,
         HttpClientModule,
+        FormsModule,
+        AngularFireModule.initializeApp(FirebaseConfig.firebase),
+        AngularFirestoreModule,
+        AngularFireAuthModule,
+        AngularFireDatabaseModule,
         routing
         
     ],
---
+++ app/environments/firebase.config.ts
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCeDXl4VxoQex2wCxzVA8VKIjyRoCXlfJk",
+  authDomain: "baby-shower-241d7.firebaseapp.com",
+  databaseURL: "https://baby-shower-241d7.firebaseio.com",
+  projectId: "baby-shower-241d7",
+  storageBucket: "baby-shower-241d7.appspot.com",
+  messagingSenderId: "397231836487",
+  appId: "1:397231836487:web:03b0d587f9cc822cda5062",
+  measurementId: "G-JG54PE1TC1"
+};
