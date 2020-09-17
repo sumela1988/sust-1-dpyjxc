9ffffa5255c17fda8f381f0d4e22853e801afbd5
sumela1988 <sumela.mixa@gmail.com>
2020-09-17 15:04:21 +0200

    1 file modified, 1 deleted

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule }    from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { FormsModule }   from '@angular/forms';
+
 
 // used to create fake backend
 import { fakeBackendProvider } from './_helpers';
@@ -20,7 +20,6 @@ import { LoginComponent } from './login';
         BrowserModule,
         ReactiveFormsModule,
         HttpClientModule,
-        FormsModule,
         routing
         
     ],
