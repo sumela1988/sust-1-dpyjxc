d0e64784090820575f035692890d84658993396d
sumela1988 <sumela.mixa@gmail.com>
2020-09-21 17:35:13 +0200

    2 files modified

--- app/products.ts
+++ app/products.ts
@@ -1,8 +1,269 @@
 export const products = [
-  {
-  name: 'onesie',
-  URL: '-',
-  picture: 'https://ae01.alicdn.com/kf/HTB1t56ScRjTBKNjSZFwq6AG4XXa8/Wholesale-Baby-Clothes-Newborn-Baby-Cotton-White-Bodysuits-Long-Sleeve-Baby-Onesie-Infant-Bebes-Jumpsuit-3.jpg_640x640.jpg'
-  }
+{
+            Nr: '1',
+            name: 'Fisher-Price Kick And Play Piano Gym',
+            picture: 'https://pricespy-75b8.kxcdn.com/product/standard/280/3454507.jpg',
+            URL: 'https://www.bol.com/nl/p/fisher-price-kick-and-play-piano-gym/9200000020487658/',
+			candidates:'-'
+        },
+        {
+            Nr: '2',
+            name: 'Philips Avent starterset newborn',
+            picture: 'https://images-na.ssl-images-amazon.com/images/I/6128pOmeXEL._AC_SX466_.jpg',
+            URL: 'https://www.prenatal.nl/eten-en-drinken/babyflesjes-en-flesspenen/philips-avent-natural-starterset-3442667.html',
+			candidates:'-'
+        },
+        {
+            Nr: '3',
+            name: 'Philips Avent magnetronsterilisator',
+            picture: 'https://cdn.webshopapp.com/shops/269916/files/334058138/image.jpg',
+            URL: 'https://www.prenatal.nl/eten-en-drinken/flessenwarmers-en-sterilisators/philips-avent-magnetronsterilisator-0952340000.html?dwvar_0952340000_kleurnaam_60=Geen%20Kleurcode&cgid=74',
+			candidates:'-'
+        },
+        {
+            Nr: '4',
+            name: 'Luvion Grand Elite 3 Connect',
+            picture: 'https://www.luvion.nl/site/wp-content/uploads/2019/05/grand-elite-3-connect-plus-set.jpg',
+            URL: 'https://www.prenatal.nl/slapen-en-thuis/slapen/babyfoons/luvion-grand-elite-3-connect-4828631.html?gclid=Cj0KCQjwhb36BRCfARIsAKcXh6EIpdeAZVTjUq8RZQ42sD5UHBqpkd4HYThKA5joMTJQ7NRW2Tv5GewaAmAREALw_wcB',
+			candidates:'-'
+        },
+        {
+            Nr: '5',
+            name: 'Diaper Champ One ',
+            picture: 'https://www.prenatal.nl/dw/image/v2/BCBH_PRD/on/demandware.static/-/Sites-prenatal-master/default/dwf3f3faa1/10/121310/121310_1213100021.jpg?sw=650&sh=650&sm=fit',
+            URL: 'https://www.prenatal.nl/bad-en-verzorging/verzorgen/luieremmers/diaper-champ-one-1213100021.html?dwvar_1213100021_kleurnaam_60=Lightgrey#q=Diaper%20Champ%20One&start=3',
+			candidates:'-'
+        },
+        {
+            Nr: '6',
+            name: 'Slaapzakje, size 56-62, long sleeve',
+            picture: 'https://cdn.shopify.com/s/files/1/0042/8343/3073/products/5811_vierkant-4_900x.jpg?v=1582633282',
+            URL: '-',
+			candidates:'-'
+        },
+        {
+            Nr: '7',
+            name: 'Slaapzakje, size 56-62, long sleeve',
+            picture: 'https://cdn.shopify.com/s/files/1/0042/8343/3073/products/5811_vierkant-4_900x.jpg?v=1582633282',
+            URL: '-',
+			candidates:'-'
+        },
+        {
+            Nr: '8',
+            name: 'Slaapzakje, size 68-80, long sleeve',
+            picture: 'https://cdn.shopify.com/s/files/1/0042/8343/3073/products/5811_vierkant-4_900x.jpg?v=1582633282',
+            URL: '-',
+			candidates:'-'
+        },
+        {
+            Nr: '9',
+            name: 'Slaapzakje, size 68-80, long sleeve',
+            picture: 'https://cdn.shopify.com/s/files/1/0042/8343/3073/products/5811_vierkant-4_900x.jpg?v=1582633282',
+            URL: '-',
+			candidates:'-'
+        },
+        {
+            Nr: '10',
+            name: 'Pants, size 56',
+            picture: 'https://www.carters.com/dw/image/v2/AAMK_PRD/on/demandware.static/-/Sites-carters_master_catalog/default/dw719a415c/productimages/17643410.jpg?sw=800&sh=1000',
+            URL: '-',
+			candidates:'-'
+        },
+        {
+            Nr: '11',
+            name: 'Pants, size 62',
+            picture: 'https://www.carters.com/dw/image/v2/AAMK_PRD/on/demandware.static/-/Sites-carters_master_catalog/default/dw719a415c/productimages/17643410.jpg?sw=800&sh=1000',
+            URL: '-',
+			candidates:'-'
+        },
+        {
+            Nr: '12',
+            name: 'Pants, size 68',
+            picture: 'https://www.carters.com/dw/image/v2/AAMK_PRD/on/demandware.static/-/Sites-carters_master_catalog/default/dw719a415c/productimages/17643410.jpg?sw=800&sh=1000',
+            URL: '-',
+			candidates:'-'
+        },
+        {
+            Nr: '13',
+            name: 'Pants, size 72',
+            picture: 'https://www.carters.com/dw/image/v2/AAMK_PRD/on/demandware.static/-/Sites-carters_master_catalog/default/dw719a415c/productimages/17643410.jpg?sw=800&sh=1000',
+            URL: '-',
+			candidates:'-'
+        },
+        {
+            Nr: '14',
+            name: 'Long sleeve t-shirts, size 56',
+            picture: 'https://ae01.alicdn.com/kf/HTB1qdNbXcnrK1RjSspkq6yuvXXaK.jpg_q50.jpg',
+            URL: '-',
+			candidates:'-'
+        },
+        {
+            Nr: '15',
+            name: 'long sleeve t-shirts, size 62',
+            picture: 'https://ae01.alicdn.com/kf/HTB1qdNbXcnrK1RjSspkq6yuvXXaK.jpg_q50.jpg',
+            URL: '-',
+			candidates:'-'
+        },
+        {
+            Nr: '16',
+            name: 'Long sleeve t-shirts, size 68',
+            picture: 'https://ae01.alicdn.com/kf/HTB1qdNbXcnrK1RjSspkq6yuvXXaK.jpg_q50.jpg',
+            URL: '-',
+			candidates:'-'
+        },
+        {
+            Nr: '17',
+            name: 'Long sleeve t-shirts, size 72',
+            picture: 'https://ae01.alicdn.com/kf/HTB1qdNbXcnrK1RjSspkq6yuvXXaK.jpg_q50.jpg',
+            URL: '-',
+			candidates:'-'
+        },
+        {
+            Nr: '18',
+            name: 'Onesies long sleeve, size 56',
+            picture: 'https://cdn.shopify.com/s/files/1/0074/6402/6227/products/itqumkztrjqhf30gv7oh.jpg?v=1583440852',
+            URL: '-',
+			candidates:'-'
+        },
+        {
+            Nr: '19',
+            name: 'Onesies long sleeve, size 62',
+            picture: 'https://cdn.shopify.com/s/files/1/0074/6402/6227/products/itqumkztrjqhf30gv7oh.jpg?v=1583440852',
+            URL: '-',
+			candidates:'-'
+        },
+        {
+            Nr: '20',
+            name: 'Onesies long sleeve, size 68',
+            picture: 'https://cdn.shopify.com/s/files/1/0074/6402/6227/products/itqumkztrjqhf30gv7oh.jpg?v=1583440852',
+            URL: '-',
+			candidates:'-'
+        },
+        {
+            Nr: '21',
+            name: 'Onesies long sleeve, size 72',
+            picture: 'https://cdn.shopify.com/s/files/1/0074/6402/6227/products/itqumkztrjqhf30gv7oh.jpg?v=1583440852',
+            URL: '-',
+			candidates:'-'
+        },
+        {
+            Nr: '22',
+            name: 'Jacket, size 62 ',
+            picture: 'https://xcdn.next.co.uk/COMMON/Items/Default/Default/ItemImages/Search/224x336/569547.jpg',
+            URL: '-',
+			candidates:'-'
+        },
+        {
+            Nr: '23',
+            name: 'Jacket, size 68',
+            picture: 'https://xcdn.next.co.uk/COMMON/Items/Default/Default/ItemImages/Search/224x336/569547.jpg',
+            URL: '-',
+			candidates:'-'
+        },
+        {
+            Nr: '25',
+            name: 'Sweaters/ vests size 62',
+            picture: 'https://ae01.alicdn.com/kf/Ha785eb6f39b14ceb8b79a9b64b6d3e98G/2020-new-winter-hedging-thick-long-sleeved-sweater-children-sweater-boys-cotton-Pullover-boy-baby-sweater.jpg',
+            URL: '-',
+			candidates:'-'
+        },
+        {
+            Nr: '26',
+            name: 'Sweaters/ vests size 68',
+            picture: 'https://ae01.alicdn.com/kf/Ha785eb6f39b14ceb8b79a9b64b6d3e98G/2020-new-winter-hedging-thick-long-sleeved-sweater-children-sweater-boys-cotton-Pullover-boy-baby-sweater.jpg',
+            URL: '-',
+			candidates:'-'
+        },
+        {
+            Nr: '27',
+            name: 'Sweaters/ vests size 72',
+            picture: 'https://ae01.alicdn.com/kf/Ha785eb6f39b14ceb8b79a9b64b6d3e98G/2020-new-winter-hedging-thick-long-sleeved-sweater-children-sweater-boys-cotton-Pullover-boy-baby-sweater.jpg',
+            URL: '-',
+			candidates:'-'
+        },
+        {
+            Nr: '28',
+            name: 'Booties ',
+            picture: 'https://static2.mumzworld.com/media/catalog/product/cache/6/image/9df78eab33525d08d6e5fb8d27136e95/s/m/smb-tigss2026bl-config-smart-baby-baby-boy-booties-blue-1599503711.jpg',
+            URL: '-',
+			candidates:'-'
+        },
+        {
+            Nr: '29',
+            name: 'Pyjamas size 62',
+            picture: 'https://excellent.gr/images/formakia-vamvakera-ekrou-mple-rige-me-saliara-02726-192-1.jpg',
+            URL: '-',
+			candidates:'-'
+        },
+        {
+            Nr: '30',
+            name: 'Pyjamas size 68',
+            picture: 'https://excellent.gr/images/formakia-vamvakera-ekrou-mple-rige-me-saliara-02726-192-1.jpg',
+            URL: '-',
+			candidates:'-'
+        },
+        {
+            Nr: '31',
+            name: 'Socks (0-3 months)',
+            picture: 'https://i.pinimg.com/originals/32/12/91/321291c9a7245391c81b9006ba92b4ca.jpg',
+            URL: '-',
+			candidates:'-'
+        },
+        {
+            Nr: '32',
+            name: 'Socks (3-6 months)',
+            picture: 'https://i.pinimg.com/originals/32/12/91/321291c9a7245391c81b9006ba92b4ca.jpg',
+            URL: '-',
+			candidates:'-'
+        },
+        {
+            Nr: '33',
+            name: 'Teddy onesie, size 62 ',
+            picture: 'https://www.dhresource.com/0x0/f2/albu/g7/M00/D7/8B/rBVaSlve-kKAZ14uAADeX5BuzG8157.jpg/baby-rompers-winter-warm-long-sleeve-coral.jpg',
+            URL: '-',
+			candidates:'-'
+        },
+        {
+            Nr: '34',
+            name: 'Teddy onesie, size 68',
+            picture: 'https://www.dhresource.com/0x0/f2/albu/g7/M00/D7/8B/rBVaSlve-kKAZ14uAADeX5BuzG8157.jpg/baby-rompers-winter-warm-long-sleeve-coral.jpg',
+            URL: '-',
+			candidates:'-'
+        },
+        {
+            Nr: '35',
+            name: 'Teddy onesie, size 72',
+            picture: 'https://www.dhresource.com/0x0/f2/albu/g7/M00/D7/8B/rBVaSlve-kKAZ14uAADeX5BuzG8157.jpg/baby-rompers-winter-warm-long-sleeve-coral.jpg',
+            URL: '-',
+			candidates:'-'
+        },
+        {
+            Nr: '36',
+            name: 'Manicure set',
+            picture: 'https://ae01.alicdn.com/kf/HTB1wZw0SpXXXXaiXFXXq6xXFXXXi/Baby-Grooming-Set-Baby-Healthcare-Kit-Newborn-Essential-Daily-Care-for-Infant-Manicure-Set-for-Baby.jpg',
+            URL: '-',
+			candidates:'-'
+        },
+        {
+            Nr: '37',
+            name: 'Care products: Soap-free babysoap, baby shampoo, babyointment, babylotion, baby oil, hair lotion',
+            picture: 'https://4.imimg.com/data4/DX/AO/MY-4573598/baby-care-products-kit-500x500.jpg',
+            URL: '-',
+			candidates:'-'
+        },
+        {
+            Nr: '38',
+            name: 'Hydrofiel luiers',
+            picture: 'https://s.s-bol.com/imgbase0/imagebase3/large/FC/2/5/4/9/9200000105769452.jpg',
+            URL: '-',
+			candidates:'-'
+        },
+        {
+            Nr: '39',
+            name: 'Hydrofiel washhands ',
+            picture: 'https://www.mamaloesbabysjop.nl/media/catalog/product/cache/1/image/1024x/9df78eab33525d08d6e5fb8d27136e95/b/r/briljant_spots_iron_hydrofiel_washandjes_3_stuks_83r.jpg',
+            URL: '-',
+			candidates:'-'
+        }
 ]
 
